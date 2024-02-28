5ee5353d10265ebad8b0e85b456c192ea4ed0a71
TulioToledoB <tuliontoledobigon2@gmail.com>
2024-02-28 11:15:23 +0100

    entrega Exam

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            animator.SetTrigger("AttackTrigger");
+        }
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            animator.SetTrigger("HurtTrigger");
+        }
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            animator.SetTrigger("DieTrigger");
+        }
+
+        // Control para que player pueda mirar hacia la izquierda o derecha
+        float inputX = Input.GetAxis("Horizontal");
+        if (inputX != 0)
+        {
+            // Se multiplica por -1 para que la dirección sea consistente con las teclas "A" y "D"
+            transform.localScale = new Vector3(Mathf.Sign(inputX) * -1, 1, 1);
+        }
+    }
+}
+
+
